808c7eadc3b81f7e279f528a5143877747c1a1d2
Inesh <i08polymath@gmail.com>
2022-08-14 19:06:22 +0530

    JS FILE UPDATE
    modified array into a 2d array
    rendered in matrix format using nested forEach loop
    added click functionality using arrow function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let gameboard_holder = (() => {
-    let gameboard = ['x','o','x','x','o','x','o','x','o'];
+    let gameboard = [['x','o','x'],['x','o','x'],['o','x','o']];
     return { gameboard };
 })();
 
@@ -13,18 +13,43 @@ let controller = (() => {
 
 let display_container = (() => {
     let display_area = document.querySelector('.display-area');
-    
+
+    // let display = () => {
+    //     gameboard_holder.gameboard.forEach(board_element => {
+    //         let cur_board_el = document.createElement('div');
+    //         cur_board_el.textContent = board_element;
+
+    //         display_area.appendChild(cur_board_el);
+    //     });
+    // }
+
     let display = () => {
         gameboard_holder.gameboard.forEach(board_element => {
-            let cur_board_el = document.createElement('div');
-            cur_board_el.textContent = board_element;
-    
-            display_area.appendChild(cur_board_el);
-        });
+            let rowEl = document.createElement('div');
+            rowEl.className = 'row rw-el';
+
+            board_element.forEach(symbol => {
+                let symbolEl = document.createElement('div');
+                symbolEl.className = 'col symbol-el';
+                symbolEl.textContent = symbol;
+
+                // adding functionality
+                symbolEl.addEventListener('click', () => {
+                    symbolEl.style = "background-color: blue;";
+                });
+                symbolEl.addEventListener('dblclick', () => {
+                    symbolEl.style = "background-color: white;";
+                });
+
+                rowEl.appendChild(symbolEl);
+            });
+
+            display_area.appendChild(rowEl);
+        })
     }
 
     return {display};
-    
+
 })();
 
 display_container.display();
